ef805372132e0fa6822c45e0c94df6adc1763b79
robin <robin@ansvia.com>
2013-11-07 22:48:42 +0700

    add fractal finder

---
+++ src/main/scala/com/ansvia/manticore/FractalFinder.scala
@@ -0,0 +1,18 @@
+package com.ansvia.manticore
+
+
+object FractalFinder {
+    
+    
+    def usage(){
+        println("Usage:\n")
+        println("      ./ffind [CSV-FILE]")
+        sys.exit(2)
+    }
+
+    
+    def main(args:Array[String]){
+        
+    }
+    
+}
